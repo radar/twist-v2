eee813110783adaf7a3b8b79f0098ab019d22a4e
Ryan Bigg <ryanbigg@cultureamp.com>
2018-04-20 08:38:13 +1000

    unless token over blank

--- backend/apps/web/controllers/graphql/run.rb
+++ backend/apps/web/controllers/graphql/run.rb
@@ -35,7 +35,7 @@ module Web::Controllers::Graphql
     def find_current_user(token)
       return unless token
       token = token.split.last
-      return if token.blank?
+      return unless token
       payload, _headers = JWT.decode token, ENV.fetch('AUTH_TOKEN_SECRET'), true, { algorithm: 'HS256' }
       user_repo = UserRepository.new
       user_repo.find_by_email(payload["email"])
