9b424bfc42a44c566dbc63ad5efd8c9605373757
Ryan Bigg <ryanbigg@cultureamp.com>
2018-05-02 14:51:25 +1000

    Add transaction test for Books::Create

---
+++ backend/spec/web/transactions/books/create_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe Web::Transactions::Books::Create do
+  context 'when given valid parameters' do
+    let(:book_repo) { double(BookRepository) }
+    subject { described_class.new(book_repo: book_repo) }
+
+    it 'creates a book' do
+      expect(book_repo).to receive(:create_with_branches).with(
+        title: "Exploding Rails",
+        permalink: "exploding-rails",
+        format: "markdown",
+        branches: [
+          {
+            name: "master",
+            default: true
+          }
+        ]
+      )
+
+      subject.(
+        title: "Exploding Rails",
+        default_branch: "master",
+        format: "markdown"
+      )
+    end
+  end
+end
