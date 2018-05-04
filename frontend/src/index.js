1d8ce3b3b45b55d6f7af09c57a5cec1d6afcb0bb
Ryan Bigg <ryanbigg@cultureamp.com>
2018-05-04 19:42:36 +1000

    Remove bootstrap from frontend app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,4 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.css'
-
 ReactDOM.render(<App />, document.getElementById('root'))
