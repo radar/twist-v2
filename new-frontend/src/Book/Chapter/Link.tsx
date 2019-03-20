6be8d5d4545bb11cc1418e13ec69a56e39ef8927
Ryan Bigg <me@ryanbigg.com>
2019-03-21 08:54:38 +1100

    Put laquo and raquos in right place

--- new-frontend/src/Book/Chapter/Link.tsx
+++ new-frontend/src/Book/Chapter/Link.tsx
@@ -3,27 +3,33 @@ import { Link } from "react-router-dom";
 import { NavigationalChapter, chapterPositionAndTitle } from "./index"
 
 type ChapterLinkProps = NavigationalChapter & {
-  icon: string,
+  direction: "back" | "forward",
 }
 
 class ChapterLink extends Component<ChapterLinkProps> {
   render() {
-    const { icon, part, position, title, permalink, bookPermalink } = this.props
+    const { id, direction, part, position, title, permalink, bookPermalink } = this.props
+    if (id === undefined) { return null }
 
     const text = chapterPositionAndTitle(part, position, title)
+    const path = `/books/${bookPermalink}/chapters/${permalink}`
 
-    return <Link to={`/books/${bookPermalink}/chapters/${permalink}`}>{icon} {text}</Link>
+    if (direction == "back") {
+      return <Link to={path}>« {text}</Link>
+    } else {
+      return <Link to={path}>{text} »</Link>
+    }
   }
 }
 
 export class PreviousChapterLink extends Component<NavigationalChapter> {
   render() {
-    return <ChapterLink {...this.props} icon={"&laquo;"} />
+    return <ChapterLink {...this.props} direction="back" />
   }
 }
 
 export class NextChapterLink extends Component<NavigationalChapter> {
   render() {
-    return <ChapterLink {...this.props} icon={"&raquo;"} />
+    return <ChapterLink {...this.props} direction="forward" />
   }
 }
