7419bc843c722a29ebf7aec3b8b47ae4fa6fa92b
Ryan Bigg <me@ryanbigg.com>
2020-02-23 21:03:32 +1100

    Stringify ref

--- backend/lib/twist_web/controllers/books/receive.rb
+++ backend/lib/twist_web/controllers/books/receive.rb
@@ -14,7 +14,7 @@ module Twist
               return
             end
 
-            branch = find_or_create_branch(book.id, payload[:ref])
+            branch = find_or_create_branch(book.id, payload["ref"]])
             worker = case book.format
                     when "markdown"
                       Twist::Markdown::BookWorker
