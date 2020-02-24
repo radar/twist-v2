f171b8053fab9c33bc98c45d967b95055a202c65
Ryan Bigg <me@ryanbigg.com>
2020-02-24 16:06:54 +1100

    Downcase incoming state for elements_with_notes

--- backend/lib/twist_web/graphql/query_type.rb
+++ backend/lib/twist_web/graphql/query_type.rb
@@ -37,7 +37,7 @@ module Twist
 
         def elements_with_notes(book_permalink:, state:)
           book = context[:book_repo].find_by_permalink(book_permalink)
-          notes = context[:book_note_repo].by_book_and_state(book.id, state)
+          notes = context[:book_note_repo].by_book_and_state(book.id, state.downcase)
 
           context[:element_repo].by_ids(notes.to_a.map(&:element_id).uniq)
         end
