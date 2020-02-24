0324d2a5ed713efb61be816345055b9230da7463
Ryan Bigg <me@ryanbigg.com>
2020-02-24 16:06:36 +1100

    Add create command to comment repo

--- backend/lib/twist/repositories/comment_repo.rb
+++ backend/lib/twist/repositories/comment_repo.rb
@@ -1,6 +1,8 @@
 module Twist
   module Repositories
     class CommentRepo < Twist::Repository[:comments]
+      commands :create, use: :timestamps, plugins_options: { timestamps: { timestamps: %i(created_at updated_at) } }
+
       def by_note_id(id)
         comments.where(note_id: id).to_a
       end
