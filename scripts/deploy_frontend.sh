b3338c79a5d0b859ea3c32eac701eba8f03b580d
Ryan Bigg <me@ryanbigg.com>
2020-02-24 16:09:14 +1100

    Correct path in deploy script

--- scripts/deploy_frontend.sh
+++ scripts/deploy_frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/code/hanami/twist-v2/frontend
+cd ~/code/twist-v2/frontend
 yarn build
 aws s3 sync build s3://twistbooks.com --acl public-read --delete
 aws cloudfront create-invalidation --distribution-id E16VNKOCFPXCL3 --paths '/*'
