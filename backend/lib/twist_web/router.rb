eaa627cd0ff58b36fe732331cf08d540cf3862d0
Ryan Bigg <me@ryanbigg.com>
2020-02-24 11:35:04 +1100

    Use sidekiq in router in dev env

--- backend/lib/twist_web/router.rb
+++ backend/lib/twist_web/router.rb
@@ -1,6 +1,11 @@
 module Twist
   module Web
     Router = Hanami::Router.new do
+      if ENV['APP_ENV'] == "development"
+        require 'sidekiq/web'
+        mount Sidekiq::Web, at: '/sidekiq'
+      end
+
       post '/graphql', to: Controllers::Graphql::Run
       options '/graphql', to: Controllers::Graphql::Run
 
