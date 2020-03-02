f8fd14ffa1460371252453eee404d7bda3434bcf
Ryan Bigg <me@ryanbigg.com>
2020-03-02 12:46:50 +1100

    Bump capistrano

--- backend/config/deploy.rb
+++ backend/config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.12.0"
 
 set :application, "twist"
 set :repo_url, "git@github.com:radar/twist-v2.git"
