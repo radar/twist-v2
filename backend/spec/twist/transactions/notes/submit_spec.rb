de20e70a87c340b0f05ba404806f91bea12bc1f7
Ryan Bigg <me@ryanbigg.com>
2020-03-31 11:11:35 +1100

    Fix note submit spec

--- backend/spec/twist/transactions/notes/submit_spec.rb
+++ backend/spec/twist/transactions/notes/submit_spec.rb
@@ -2,18 +2,21 @@ require "spec_helper"
 
 describe Twist::Transactions::Notes::Submit do
   context 'when given valid parameters' do
+    let(:book_repo) { double(Twist::Repositories::BookRepo) }
     let(:note_repo) { double(Twist::Repositories::NoteRepo) }
     let(:book_note_repo) { double(Twist::Repositories::BookNoteRepo) }
     let(:user) { double(Twist::User, id: 1) }
 
     subject do
       described_class.new(
+        book_repo: book_repo,
         book_note_repo: book_note_repo,
-        note_repo: note_repo
+        note_repo: note_repo,
       )
     end
 
     before do
+      allow(book_repo).to receive(:find_by_permalink) { double(id: 1) }
       allow(book_note_repo).to receive(:count_for_book) { 1 }
     end
 
@@ -27,7 +30,7 @@ describe Twist::Transactions::Notes::Submit do
       )
 
       result = subject.(
-        book_id: 1,
+        book_permalink: "example-book",
         user_id: user.id,
         element_id: 1,
         text: "New note goes here"
