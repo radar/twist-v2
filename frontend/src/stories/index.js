d13a11a8808bd0316bc4bc8091607f082bbc3aa6
Ryan Bigg <me@ryanbigg.com>
2020-04-23 10:57:35 +1000

    Update Storybook + separate stories

--- frontend/src/stories/index.js
+++ frontend/src/stories/index.js
@@ -2,58 +2,13 @@ import React from "react";
 
 import { storiesOf, addDecorator } from "@storybook/react";
 // import { action } from '@storybook/addon-actions';
-import { linkTo } from "@storybook/addon-links";
 
 import "../App.scss";
 
-addDecorator(
-  StoryRouter({
-    "/": linkTo("Books", "list"),
-    "/books/:id": linkTo("Book", "show"),
-    "/books/:bookId/chapters/:chapterId": linkTo("Chapter", "show")
-  })
-);
-
 import { Book } from "../Book";
-import { Books } from "../Books";
 import { Chapter } from "../Book/Chapter";
 import { Form as NoteForm } from "../Book/Chapter/Notes/Form";
 import { NoteList } from "../Book/Notes/List";
-
-import { ElementWithNotesProps } from "../Book/Notes/types";
-
-storiesOf("Books", module).add("list", () => {
-  const books = [
-    {
-      id: 1,
-      title: "Markdown Book Test",
-      permalink: "markdown-book-test",
-      blurb: "For testing Markdown books"
-    }
-  ];
-  return <Books books={books} />;
-});
-
-storiesOf("Book", module).add("show", () => {
-  const defaultBranch = {
-    frontmatter: [
-      {
-        id: "1",
-        title: "Introduction",
-        permalink: "introduction"
-      }
-    ],
-    mainmatter: [],
-    backmatter: []
-  };
-  return (
-    <Book
-      title="Markdown Book Test"
-      permalink="markdown-book-test"
-      defaultBranch={defaultBranch}
-    />
-  );
-});
 
 storiesOf("Book/Notes", module).add("list", () => {
   const elementsWithNotes = [
@@ -69,9 +24,9 @@ storiesOf("Book/Notes", module).add("list", () => {
           user: {
             id: "1",
             email: "me@ryanbigg.com",
-            name: "Ryan Bigg"
-          }
-        }
+            name: "Ryan Bigg",
+          },
+        },
       ],
       chapter: {
         title: "Introduction",
@@ -80,10 +35,10 @@ storiesOf("Book/Notes", module).add("list", () => {
         commit: {
           sha: "abc1234",
           branch: {
-            name: "master"
-          }
-        }
-      }
+            name: "master",
+          },
+        },
+      },
     },
     {
       id: "2",
@@ -97,9 +52,9 @@ storiesOf("Book/Notes", module).add("list", () => {
           user: {
             id: "1",
             email: "me@ryanbigg.com",
-            name: "Ryan Bigg"
-          }
-        }
+            name: "Ryan Bigg",
+          },
+        },
       ],
       chapter: {
         title: "Introduction",
@@ -108,11 +63,11 @@ storiesOf("Book/Notes", module).add("list", () => {
         commit: {
           sha: "abc1234",
           branch: {
-            name: "master"
-          }
-        }
-      }
-    }
+            name: "master",
+          },
+        },
+      },
+    },
   ];
   return (
     <div className="main">
@@ -140,26 +95,26 @@ storiesOf("Book/Chapter", module)
           id: "1",
           content: "<h1>In the beginning</h1>",
           tag: "h1",
-          noteCount: 1
+          noteCount: 1,
         },
         {
           id: "2",
           content: "<p>This is a test of the markdown processing system</p>",
           tag: "p",
-          noteCount: 0
+          noteCount: 0,
         },
         {
           id: "3",
           content: "<h2>This is a new section</h2>",
           tag: "h2",
-          noteCount: 1
+          noteCount: 1,
         },
         {
           id: "4",
           content: "<p>And here's some text for that section.</p>",
           tag: "p",
-          noteCount: 0
-        }
+          noteCount: 0,
+        },
       ],
       sections: [
         {
@@ -170,13 +125,13 @@ storiesOf("Book/Chapter", module)
             {
               id: "2",
               link: "#this-is-a-new-section",
-              title: "This is a new section"
-            }
-          ]
-        }
+              title: "This is a new section",
+            },
+          ],
+        },
       ],
       nextChapter: null,
-      previousChapter: null
+      previousChapter: null,
     };
     return <Chapter {...chapter} />;
   })
@@ -184,7 +139,7 @@ storiesOf("Book/Chapter", module)
     const formProps = {
       elementId: "1",
       submitNote: () => {},
-      noteSubmitted: () => {}
+      noteSubmitted: () => {},
     };
     return (
       <div className="main">
