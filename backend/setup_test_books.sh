8a503ab682bcef5ef6ccc6da52bb0f78084f9b62
Ryan Bigg <me@ryanbigg.com>
2020-06-15 19:52:52 +1000

    Add script to setup books

---
+++ backend/setup_test_books.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -rf spec/twist/repos/radar/*
+
+git clone git@github.com:radar/asciidoc_book_test spec/twist/repos/radar/asciidoc_book_test
+git clone git@github.com:radar/markdown_book_test spec/twist/repos/radar/markdown_book_test
