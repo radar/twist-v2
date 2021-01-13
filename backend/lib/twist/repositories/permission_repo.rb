19901856a4bb8429f2807f7eaab1c86d162bcf31
Ryan Bigg <me@ryanbigg.com>
2021-01-13 13:06:25 +1100

    Only create a permission record if one does not already exist

--- backend/lib/twist/repositories/permission_repo.rb
+++ backend/lib/twist/repositories/permission_repo.rb
@@ -3,6 +3,10 @@ module Twist
     class PermissionRepo < Twist::Repository[:permissions]
       commands :create
 
+      def find_by_book_id_and_user_id(book_id, user_id)
+        permissions.where(book_id: book_id, user_id: user_id).first
+      end
+
       def user_authorized_for_book?(user, book)
         return true if ENV['BYPASS_PERMISSIONS']
         permissions.where(
--- backend/lib/twist/transactions/invitations/invite.rb
+++ backend/lib/twist/transactions/invitations/invite.rb
@@ -3,12 +3,14 @@ module Twist
     module Invitations
       class Invite
         include Dry::Transaction
-        include Twist::Import["permission_repo"]
+        include Twist::Import["repositories.permission_repo"]
 
         step :invite
 
         def invite(book_id:, user_id:)
-          Success(permission_repo.create(book_id: book_id, user_id: user_id))
+          permission = permission_repo.find_by_book_id_and_user_id(book_id, user_id)
+          permission ||= permission_repo.create(book_id: book_id, user_id: user_id)
+          Success(permission)
         end
       end
     end
