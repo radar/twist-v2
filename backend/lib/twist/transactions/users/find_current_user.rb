4326dd333d6fc5ba4d7a5afe1f1ec7a4bc086398
Ryan Bigg <me@ryanbigg.com>
2022-06-30 09:08:25 +1000

    Add name for users

--- backend/lib/twist/transactions/users/find_current_user.rb
+++ backend/lib/twist/transactions/users/find_current_user.rb
@@ -11,7 +11,7 @@ module Twist
           return Failure("no token specified") unless token
 
           payload, _headers = JWT.decode token, ENV.fetch('AUTH_TOKEN_SECRET'), true, algorithm: 'HS256'
-          user = user_repo.find_by_email(payload["email"]) || user_repo.create(email: payload["email"])
+          user = user_repo.find_by_email(payload["email"]) || user_repo.create(email: payload["email"], name: payload["email"])
           user ? Success(user) : Failure("no user found")
         end
       end
